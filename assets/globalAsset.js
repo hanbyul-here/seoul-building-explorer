e9c2f9ca474a493b67f282dd55066c911759ba68
Hanbyul Jo <hanbyul@nyu.edu>
2017-05-08 23:42:10 -0400

    added nsdi link

--- assets/globalAsset.js
+++ assets/globalAsset.js
@@ -33,8 +33,8 @@ var globalAsset = {
     kr: '서울 건물 나이 지도'
   },
   description: {
-    en: 'This map shows when more than 700,000 buildings in Seoul were built.',
-    kr: '본 지도는 서울 내 70만 대 이상의 건물이 언제 지어졌는지 보여주기 위해 만들어졌습니다. 원본 데이터는 국가정보공간포털에서 찾으실 수 있습니다.'
+    en: 'This map shows when more than 700,000 buildings in Seoul were built. The data is from <a href="http://openapi.nsdi.go.kr/nsdi/eios/ServiceDetail.do?svcSe=F&svcId=F010">NSDI</a>.',
+    kr: '본 지도는 서울 내 70만 대 이상의 건물이 언제 지어졌는지 보여주기 위해 만들어졌습니다. 원본 데이터는 <a href="http://openapi.nsdi.go.kr/nsdi/eios/ServiceDetail.do?svcSe=F&svcId=F010">국가정보공간포털</a>에서 찾을 수 있습니다.'
   },
   viridis :["#440154", "#482475", "#414287", "#355e8d", "#2a768e", "#218e8d", "#21a585", "#3dbc74", "#70cf57", "#b0dd2f"]
 }
